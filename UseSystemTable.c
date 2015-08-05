e7aa7b236bac910dfe131b16ee9e9aff07bf630e
PassP <DeviceObject@gmail.com>
2015-08-06 04:58:58 +0800

    fixed UseSystemTable.c

--- UseSystemTable.c
+++ UseSystemTable.c
@@ -5,12 +5,19 @@ EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle,IN EFI_SYSTEM_TABLE *SystemTable)
 	EFI_STATUS Status;
 	UINTN Index;
 	EFI_INPUT_KEY Key;
-	CHAR16 StrBuffer[3] = {0};
+	CHAR16 StrBuffer[3];
+	//__asm int 3
 	SystemTable->BootServices->WaitForEvent(1,&SystemTable->ConIn->WaitForKey,&Index);
 	//wait input
 	Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn,&Key);
+	//__asm int 3
 	StrBuffer[0] = Key.UnicodeChar;
+	if (StrBuffer[0] == L'q')
+	{
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Input \'q\'\n");
+	}
 	StrBuffer[1] = '\n';
+
 	SystemTable->ConOut->OutputString(SystemTable->ConOut,StrBuffer);
 	return EFI_SUCCESS;
 }
