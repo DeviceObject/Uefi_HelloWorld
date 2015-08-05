eb64015176cad89527e2bbee4adac6e35d313f57
PassP <DeviceObject@gmail.com>
2015-08-05 22:29:39 +0800

    update file

---
+++ UefiMain.c
@@ -0,0 +1,6 @@
+#include <Uefi.h>
+EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle,IN EFI_SYSTEM_TABLE *SystemTable)
+{
+	SystemTable->ConOut->OutputString(SystemTable->ConOut,L"HelloWorld\n");
+	return EFI_SUCCESS;
+}
